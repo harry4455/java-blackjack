24b1d86211da0efc783ea98114afc00def190b9d
harry4455 <xoxolittlemonster4455@gmail.com>
2025-12-04 15:57:00 +0900

    feat: 카드에 따라 1~11 까지의 점수로 계산한다

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,3 +1,5 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -7,9 +9,13 @@ import static org.assertj.core.api.Assertions.*;
 class CardTest {
 
     @Test
-    void getCard() {
-        Deck deck = new Deck();
-
+    @DisplayName("카드에 따라 1~11 까지의 점수로 계산한다")
+    void checkCardScore() {
+        Card card = new Card("Hearts", "A");
+        assertThat(card.getScore(card)).isEqualTo(11);
     }
+
+//    @Test
+//    @DisplayName("ACE는 조건에 따라 1 또는 11로 계산한다")
 
 }
