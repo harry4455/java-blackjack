dfab004e564c6ae0957282861cef0ded4432a854
harry4455 <xoxolittlemonster4455@gmail.com>
2025-12-04 15:58:30 +0900

    docs: README 및 initial setting 수정

---
+++ src/test/java/State/HitTest.java
@@ -0,0 +1,13 @@
+package State;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class HitTest {
+
+    @Test
+    @DisplayName("")
+    void Hit() {
+
+    }
+}
